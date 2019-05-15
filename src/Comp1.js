25d12cbde52fec3730b177748213eadb00f42330
Amit Arbel <arbel.amit@gmail.com>
2019-05-15 23:28:14 +0300

    Initial commit

---
+++ src/Comp1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Comp1() {
+  return (
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+  );
+}
+
+export default Comp1;
